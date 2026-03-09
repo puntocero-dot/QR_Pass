d7b9526adc4fb230d2a84e4bff0f8a5a6235111a
Punto Cero <admin@puntocero.dev>
2026-03-08 18:20:23 -0600

    fix: cleanup server.js duplicates and finalize stability fixes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const express = require('express');
-const cors = require('cors');
 const path = require('path');
 const { initDB, seedDemoData } = require('./db');
 
